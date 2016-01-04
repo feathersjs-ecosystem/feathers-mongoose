7d611c6eac1893da94ede6a4541e082918db73da
Eric Kryski <e.kryski@gmail.com>
2016-01-04 02:59:10 -0700

    no longer exposing mongoose

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import * as hooks from './hooks';
 import service from './service';
-import mongoose from 'mongoose';
 
 export { hooks };
-export { mongoose };
 export { service };
 export default service;
