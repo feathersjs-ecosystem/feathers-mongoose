9ab940b061dd0f85ebe0b503cd31dca2f32e24ab
Eric Kryski <e.kryski@gmail.com>
2016-02-21 15:25:05 -0700

    udpating example

--- example/app.js
+++ example/app.js
@@ -1,10 +1,13 @@
-var feathers = require('feathers');
-var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
-var mongooseService = require('../lib');
+const feathers = require('feathers');
+const rest = require('feathers-rest');
+const socketio = require('feathers-socketio');
+const errors = require('feathers-errors');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const mongooseService = require('../lib');
 
 // Require your models
-var Todo = require('./models/todo');
+const Message = require('./models/message');
 
 // Tell mongoose to use native promises
 // See http://mongoosejs.com/docs/promises.html
@@ -15,20 +18,20 @@ mongoose.connect('mongodb://localhost:27017/feathers');
 
 
 // Create a feathers instance.
-var app = feathers()
+const app = feathers()
   // Enable Socket.io
-  .configure(feathers.socketio())
+  .configure(socketio())
   // Enable REST services
-  .configure(feathers.rest())
+  .configure(rest())
   // Turn on JSON parser for REST services
   .use(bodyParser.json())
   // Turn on URL-encoded parser for REST services
-  .use(bodyParser.urlencoded({extended: true}))
+  .use(bodyParser.urlencoded({extended: true}));
 
 // Connect to the db, create and register a Feathers service.
-app.use('todos', mongooseService({
-  name: 'todo',
-  Model: Todo,
+app.use('messages', mongooseService({
+  name: 'message',
+  Model: Message,
   paginate: {
     default: 2,
     max: 4
@@ -36,14 +39,12 @@ app.use('todos', mongooseService({
 }));
 
 // A basic error handler, just like Express
-app.use(function(error, req, res, next){
-  res.json(error);
-});
+app.use(errors.handler());
 
 // Start the server if we're not being required in a test
 if (!module.parent) {
   app.listen(3030);
-  console.log('Feathers Todo mongoose service running on 127.0.0.1:3030');
+  console.log('Feathers Message mongoose service running on 127.0.0.1:3030');
 }
 
 module.exports = app;
