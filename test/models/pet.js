0657235c23f927f0da7c44b161d8ff12fa19a8f9
jamesjnadeau <james.nadeau@gmail.com>
2017-01-13 10:07:59 -0500

    :white_check_mark: add test for different $select types possible

--- test/models/pet.js
+++ test/models/pet.js
@@ -3,7 +3,8 @@ const Schema = mongoose.Schema;
 
 const PetSchema = new Schema({
   type: {type: String, required: true},
-  name: {type: String, required: true, unique: true}
+  name: {type: String, required: true, unique: true},
+  gender: {type: String, required: false, select: false}
 });
 
 const PetModel = mongoose.model('Pet', PetSchema);
