d047c5d24e5d6d7b393ace678c3d8624392f28d1
Eric Kryski <e.kryski@gmail.com>
2017-02-27 15:29:56 -0700

    adding better mongoose conflict error handling

--- src/error-handler.js
+++ src/error-handler.js
@@ -14,7 +14,23 @@ export default function errorHandler (error) {
       case 'DivergentArrayError':
         return Promise.reject(new errors.GeneralError(error));
       case 'MongoError':
-        if (error.code === 11000) {
+        if (error.code === 11000 || error.code === 11001) {
+          // NOTE (EK): Error parsing as discussed in this github thread
+          // https://github.com/Automattic/mongoose/issues/2129
+          const key = error.message.match(/_?([a-zA-Z]*)_?\d?\s*dup key/i)[1];
+          let value = error.message.match(/\s*dup key:\s*\{\s*:\s*"?([a-zA-Z0-9'().]+)"?\s*\}/i)[1];
+
+          if (value === 'null') {
+            value = null;
+          } else if (value === 'undefined') {
+            value = undefined;
+          }
+
+          error.message = `${key} '${value}' already exists.`;
+          error.errors = {
+            [key]: value
+          };
+
           return Promise.reject(new errors.Conflict(error));
         }
 
