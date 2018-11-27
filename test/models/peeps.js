1d42a42023b2c95c11595a89a5dceb024a1f85a5
David Luecke <daff@neyeon.com>
2018-11-27 08:36:35 -0800

    Fix deprecation warnings and update dependencies (#275)

--- test/models/peeps.js
+++ test/models/peeps.js
@@ -4,8 +4,8 @@ const Schema = mongoose.Schema;
 const PeepsSchema = new Schema({
   name: { type: String, required: true },
   age: { type: Number },
-  created: {type: Boolean, 'default': false},
-  time: {type: Number}
+  created: { type: Boolean, 'default': false },
+  time: { type: Number }
 });
 
 module.exports = mongoose.model('Peeps', PeepsSchema);
--- test/models/todo.js
+++ test/models/todo.js
@@ -2,10 +2,10 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var TodoSchema = new Schema({
-  text: {type: String, required: true},
-  complete: {type: Boolean, 'default': false},
-  createdAt: {type: Date, 'default': Date.now},
-  updatedAt: {type: Date, 'default': Date.now}
+  text: { type: String, required: true },
+  complete: { type: Boolean, 'default': false },
+  createdAt: { type: Date, 'default': Date.now },
+  updatedAt: { type: Date, 'default': Date.now }
 });
 
 var TodoModel = mongoose.model('Todo', TodoSchema);
