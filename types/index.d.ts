63520cb3c2cad4b524d652c76cc0b084efbbf690
David Luecke <daff@neyeon.com>
2019-07-05 09:40:08 -0700

    Add TypeScript definitions and upgrade tests to Feathers 4

---
+++ types/index.d.ts
@@ -0,0 +1,35 @@
+// TypeScript Version: 3.0
+import { Params, Paginated, Id, NullableId, Hook } from '@feathersjs/feathers';
+import { AdapterService, ServiceOptions, InternalServiceMethods } from '@feathersjs/adapter-commons';
+import { Model } from 'mongoose';
+
+export namespace hooks {
+  function toObject(options?: any, dataField?: string): Hook;
+}
+
+export namespace transactionManager {
+  const beginTransaction: Hook;
+  const commitTransaction: Hook;
+  const rollbackTransaction: Hook;
+}
+
+export interface MongooseServiceOptions extends ServiceOptions {
+  Model: Model<any>;
+}
+
+export class Service<T = any> extends AdapterService implements InternalServiceMethods<T> {
+  Model: Model<any>;
+  options: MongooseServiceOptions;
+
+  constructor(config?: Partial<MongooseServiceOptions>);
+
+  _find(params?: Params): Promise<T | T[] | Paginated<T>>;
+  _get(id: Id, params?: Params): Promise<T>;
+  _create(data: Partial<T> | Array<Partial<T>>, params?: Params): Promise<T | T[]>;
+  _update(id: NullableId, data: T, params?: Params): Promise<T>;
+  _patch(id: NullableId, data: Partial<T>, params?: Params): Promise<T>;
+  _remove(id: NullableId, params?: Params): Promise<T>;
+}
+
+declare const mongoose: ((config?: Partial<MongooseServiceOptions>) => Service);
+export default mongoose;
---
+++ types/index.test.ts
@@ -0,0 +1,29 @@
+import { Schema, model } from 'mongoose';
+import feathers from '@feathersjs/feathers';
+import { default as mongoose, hooks, transactionManager } from 'feathers-mongoose';
+
+const MessageSchema = new Schema({
+  text: {
+    type: String,
+    required: true
+  }
+});
+const Model = model('Message', MessageSchema);
+const service = mongoose({
+  Model
+});
+
+service.Model;
+
+const app = feathers().use('/test', service);
+
+app.service('test').hooks({
+  after: {
+    all: [
+      hooks.toObject(),
+      transactionManager.beginTransaction,
+      transactionManager.commitTransaction,
+      transactionManager.rollbackTransaction
+    ]
+  }
+});
